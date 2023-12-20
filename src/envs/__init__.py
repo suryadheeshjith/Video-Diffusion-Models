452615e43413cf6cea53c6d951ad52c08ebfd621
suryadheeshjith <Surya.dheeshjith@gmail.com>
2023-12-20 18:02:51 -0500

    Removed unnecessary files

--- src/envs/__init__.py
+++
@@ -1,4 +0,0 @@
-from .multi_process_env import MultiProcessEnv
-from .wrappers import make_atari, ResizeObsWrapper
-from .single_process_env import SingleProcessEnv
-from .world_model_env import WorldModelEnv
--- src/game/__init__.py
+++
@@ -1,4 +0,0 @@
-from .agent_env import AgentEnv
-from .episode_replay_env import EpisodeReplayEnv
-from .game import Game
-from .SaveAutoregressive import SaveAutoregressive
