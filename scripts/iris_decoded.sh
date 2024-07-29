7cae720974d4e865a178334567ced3ccd77da785
suryadheeshjith <Surya.dheeshjith@gmail.com>
2024-07-29 14:59:55 -0400

    Removed workflow

--- scripts/iris_decoded.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Obvious / static arguments
-# ./.python-greene submitit_hydra.py compute/greene=1x1 compute/greene/node=rtx8000_6hrs exp=breakout_iris_decoded name="$(date +%F)-IRIS_DECODED"
-
-./.python-greene submitit_hydra.py compute/greene=1x1 compute/greene/node=rtx8000_6hrs exp=breakout_iris_decoded name="$(date +%F)-IRIS_DECODED_300"
